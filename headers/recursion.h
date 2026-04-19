579543b91ca30627a58a31bc3bb653a1c5a955b9
kebbicheanes <kebbicheanes@gmail.com>
2026-04-19 14:22:08 +0100

    Add function prototypes for recursion operations

--- headers/recursion.h
+++ headers/recursion.h
@@ -1,2 +1,16 @@
+#include <stdbool.h>
 #ifndef RECURSION_H
 #define RECURSION_H
+
+typedef File File;
+
+int countOccurence(File *f, char *name);
+File* removeOccurence(File *f, char *word);
+File* replaceOccurence(File *f, char *name, char *DoB, char *DoD);
+void namePermutation(char *name);
+void subseqName(char *word);
+void longestSubyear(char *date1, char *date2);
+int distinctSubseqWord(char *event);
+bool isPalindromeWord(char *event);
+
+#endif
