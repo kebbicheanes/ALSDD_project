675b8ac52062ee26efb7c6d6cbeee538edf2eab1
kebbicheanes <kebbicheanes@gmail.com>
2026-05-16 22:50:08 +0100

    Refactor stacks.h by removing unused structures

--- headers/stacks.h
+++ headers/stacks.h
@@ -1,31 +1,10 @@
-#include <stdbool.h>
 #ifndef STACKS_H
 #define STACKS_H
 
-typedef struct TStack {
-    char          name[100];
-    char          definition[1024];
-    char          DoB[20];   /* Date of Birth  */
-    char          DoD[256];   /* Date of Death  */
-    struct TStack *next;
-} TStack;
-
-typedef struct TList {
-    char          name[100];
-    char          definition[1024];
-    char          DoB[20];   /* Date of Birth  */
-    char          DoD[256];   /* Date of Death  */
-    struct TList *next;
-} TList;
-
-typedef struct TQueue {
-    char          name[100];
-    char          definition[1024];
-    char          DoB[20];   /* Date of Birth  */
-    char          DoD[256];   /* Date of Death  */
-    struct TQueue *next;
-} TQueue;
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "types.h"
 
 TStack* toStack(TList *merged);
 TStack* getInfoPersonality(TStack *stk, char *name);
@@ -33,13 +12,14 @@ TStack* sortNameStack(TStack *s);
 TStack* deleteName(TStack *stk, char *name);
 TStack* updateStack(TStack *stk, char *name, char *def, char *DoB, char *DoD);
 TQueue* stackToQueue(TStack *stk);
-TList* stackToList(TStack *stk);
+TList*  stackToList(TStack *stk);
 TStack* addNameStack(TStack *stk, char *name, char *definition, char *DoB, char *DoD);
 TStack* definitionStack(TStack *stk);
-TStack* pronunciationStack(TStack *stk);
-char* getSmallest(TStack *stk);
-void continuousSearch(TStack *stk);
-bool isPersonalityKilled(char *word);
-TStack* recRevStack (TStack *stk);
+TStack* pronunciationStack(TStack *stk, TStack **longStk);
+char*   getSmallest(TStack *stk);
+void    continuousSearch(TStack *stk);
+bool    isPersonalityKilled(char *word);
+TStack* recRevStack(TStack *stk);
+void    runStacks(TList *merged);
 
 #endif
