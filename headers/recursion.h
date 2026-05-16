dfe6d2f2323b756789b40ef572756791fc874145
kebbicheanes <kebbicheanes@gmail.com>
2026-05-16 22:49:19 +0100

    Update recursion.h to use FILE instead of File

--- headers/recursion.h
+++ headers/recursion.h
@@ -1,16 +1,20 @@
-#include <stdbool.h>
 #ifndef RECURSION_H
 #define RECURSION_H
 
-typedef File File;
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "types.h"
 
-int countOccurence(File *f, char *name);
-File* removeOccurence(File *f, char *word);
-File* replaceOccurence(File *f, char *name, char *DoB, char *DoD);
-void namePermutation(char *name);
-void subseqName(char *word);
-void longestSubyear(char *date1, char *date2);
-int distinctSubseqWord(char *event);
-bool isPalindromeWord(char *event);
+
+int   countOccurence(FILE *f, char *name);
+FILE* removeOccurence(FILE *f, char *word);
+FILE* replaceOccurence(FILE *f, char *name, char *DoB, char *DoD);
+void  namePermutation(char *name);
+void  subseqName(char *word);
+void  longestSubyear(char *date1, char *date2);
+int   distinctSubseqWord(char *event);
+bool  isPalindromeWord(char *event);
+void  runRecursion(FILE *f);
 
 #endif
